e753a1b87cea353152c3c913a85281918d6508ff
omar-as <omarelassaad93@gmail.com>
2023-02-27 10:33:40 +0300

    Hibernate action taken after low power

--- configuration.nix
+++ configuration.nix
@@ -34,6 +34,16 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Low Power Enable
+  services.upower = {
+      enable = true;
+      usePercentageForPolicy = true;
+      percentageLow = 40;
+      percentageCritical = 20;
+      percentageAction = 10;
+      criticalPowerAction = "Hibernate";
+    };
+  
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
