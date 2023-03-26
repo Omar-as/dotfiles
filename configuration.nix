b72bda5f8bcfb78bf73df2118a800fa447686476
omar-as <omarelassaad93@gmail.com>
2023-03-26 16:10:29 +0300

    Update

--- configuration.nix
+++ configuration.nix
@@ -22,6 +22,8 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  programs.fish.enable = true;
+
   # Enable networking
   networking.networkmanager.enable = true;
 
